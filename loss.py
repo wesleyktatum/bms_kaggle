b86be802a29dce29684fc0eb6e6aab9ac4d7ab03
Orion Dollar <orion.dollar@gmail.com>
2021-03-28 16:50:42 -0700

    split_loss

--- loss.py
+++ loss.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn
 import torch.nn.functional as F
 
-def ce_loss(targets, preds, weights):
+def ce_loss(targets, preds, alphas, weights, alpha_c):
     BCE = F.cross_entropy(preds, targets, reduction='mean', weight=weights)
-    return BCE
+    ALPHA_NORM = alpha_c * ((1. - alphas.sum(dim=1))**2).mean()
+    return BCE + ALPHA_NORM, BCE, ALPHA_NORM
--- train.py
+++ train.py
@@ -47,7 +47,7 @@ def main(args):
         already_wrote = False
     log_file = open(args.log_fn, 'a')
     if not already_wrote:
-        log_file.write('epoch,batch_idx,data_type,loss,run_time\n')
+        log_file.write('epoch,batch_idx,data_type,bce_loss,alpha_norm_loss,tot_loss,run_time\n')
     log_file.close()
 
     if args.checkpoint_fn is not None:
@@ -140,6 +140,8 @@ def train(train_loader, model, optimizer, epoch, args, batch_counter=0):
         # data_load_end = perf_counter()
         # data_load_times.append(data_load_end - data_load_start)
         avg_losses = []
+        avg_bce_losses = []
+        avg_alpha_norm_losses = []
         for j in range(args.batch_chunks):
             # chunk_start = perf_counter()
             imgs = batch_imgs[j*args.chunk_size:(j+1)*args.chunk_size,:,:,:]
@@ -162,8 +164,7 @@ def train(train_loader, model, optimizer, epoch, args, batch_counter=0):
             # postprocess_times.append(postprocess_end - postprocess_start)
 
             # calc_loss_start = perf_counter()
-            loss = ce_loss(targets, preds, args.char_weights)
-            loss += args.alpha_c * ((1. - alphas.sum(dim=1))**2).mean()
+            loss, bce, alpha_norm = ce_loss(targets, preds, alphas, args.char_weights, args.alpha_c)
             # calc_loss_end = perf_counter()
             # calc_loss_times.append(calc_loss_end - calc_loss_start)
 
@@ -173,6 +174,8 @@ def train(train_loader, model, optimizer, epoch, args, batch_counter=0):
             # backprop_times.append(backprop_end - backprop_start)
 
             avg_losses.append(loss.item())
+            avg_bce_losses.append(bce.item())
+            avg_alpha_norm_losses.append(alpha_norm.item())
 
         if args.grad_clip is not None:
             clip_gradient(optimizer, args.grad_clip)
@@ -187,15 +190,19 @@ def train(train_loader, model, optimizer, epoch, args, batch_counter=0):
         stop_time = perf_counter()
         batch_time = round(stop_time - start_time, 5)
         avg_loss = round(np.mean(avg_losses), 5)
+        avg_bce_loss = round(np.mean(avg_bce_losses), 5)
+        avg_alpha_norm_loss = round(np.mean(avg_alpha_norm_losses), 5)
         losses.append(avg_loss)
         batch_counter += 1
 
         # Log
         # write_log_start = perf_counter()
         log_file = open(args.log_fn, 'a')
-        log_file.write('{},{},{},{},{}\n'.format(epoch,
+        log_file.write('{},{},{},{},{},{},{}\n'.format(epoch,
                                                  batch_counter,
                                                  'train',
+                                                 avg_bce_loss,
+                                                 avg_alpha_norm_loss,
                                                  avg_loss,
                                                  batch_time))
         log_file.close()
@@ -238,6 +245,8 @@ def validate(val_loader, model, epoch, args, batch_counter=0):
             batch_encoded_inchis = batch_encoded_inchis.to(DEVICE)
             batch_inchi_lengths = batch_inchi_lengths.unsqueeze(1).to(DEVICE)
             avg_losses = []
+            avg_bce_losses = []
+            avg_alpha_norm_losses = []
             for j in range(args.batch_chunks):
                 imgs = batch_imgs[j*args.chunk_size:(j+1)*args.chunk_size,:,:,:]
                 encoded_inchis = batch_encoded_inchis[j*args.chunk_size:(j+1)*args.chunk_size,:]
@@ -250,22 +259,27 @@ def validate(val_loader, model, epoch, args, batch_counter=0):
                 preds = pack_padded_sequence(preds, decode_lengths, batch_first=True).data
                 targets = pack_padded_sequence(targets, decode_lengths, batch_first=True).data
 
-                loss = ce_loss(targets, preds, args.char_weights)
-                loss += args.alpha_c * ((1. - alphas.sum(dim=1))**2).mean()
+                loss, bce, alpha_norm = ce_loss(targets, preds, alphas, args.char_weights, args.alpha_c)
 
                 avg_losses.append(loss.item())
+                avg_bce_losses.append(bce.item())
+                avg_alpha_norm_losses.append(alpha_norm.item())
 
             stop_time = perf_counter()
             batch_time = round(stop_time - start_time, 5)
             avg_loss = round(np.mean(avg_losses), 5)
+            avg_bce_loss = round(np.mean(avg_bce_losses), 5)
+            avg_alpha_norm_loss = round(np.mean(avg_alpha_norm_losses), 5)
             losses.append(avg_loss)
             batch_counter += 1
 
             # Log
             log_file = open(args.log_fn, 'a')
-            log_file.write('{},{},{},{},{}\n'.format(epoch,
+            log_file.write('{},{},{},{},{},{},{}\n'.format(epoch,
                                                      batch_counter,
                                                      'val',
+                                                     avg_bce_loss,
+                                                     avg_alpha_norm_loss,
                                                      avg_loss,
                                                      batch_time))
             log_file.close()
